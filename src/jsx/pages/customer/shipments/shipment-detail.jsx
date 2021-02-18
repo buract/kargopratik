67e802e5ff07fb2421d312d63c274a30cab67349
Seçkin Sözen <31372410+SeckinSozen@users.noreply.github.com>
2021-02-18 16:08:04 +0300

    Created Shipment Detail

--- src/jsx/pages/customer/shipments/shipment-detail.jsx
+++ src/jsx/pages/customer/shipments/shipment-detail.jsx
@@ -1,7 +1,7 @@
 import React, { } from 'react';
-import Header2 from '../../../layout/header-admin';
-import Sidebar from '../../../layout/sidebar-admin';
-import PageTitle from '../../../partial/admin/customer/page-title';
+import Header2 from '../../../layout/header2';
+import Sidebar from '../../../layout/sidebar';
+import PageTitle from '../../../partial/customer/shipments/detail-page-title';
 
 import Footer2 from '../../../layout/footer2'
 import RecentShipment from "../../../partial/admin/customer/recent-shipment";
@@ -10,7 +10,7 @@ import AccountActivities from '../../../partial/admin/customer/account-activitie
 
 
 
-function CustomerDetail() {
+function ShipmentDetail() {
 
     return (
         <>
@@ -31,7 +31,7 @@ function CustomerDetail() {
                                 <Accordion defaultActiveKey="0" id="accordion-faq" className="accordion">
                                     <Card>
                                         <Accordion.Toggle as={Card.Header} >
-                                            <span className="card-head text-primary">ID: 123456</span><div><span className=" text-primary">Bakiye 12.000TL</span></div>
+                                            <span className="card-head text-primary">ID: 123456</span><div><span className=" text-primary">Gönderi Ücreti 1.200TL</span></div>
                                         </Accordion.Toggle>
                                         <Accordion.Collapse >
                                             <Card.Body></Card.Body>
@@ -39,9 +39,38 @@ function CustomerDetail() {
                                     </Card>
                                     <Card>
                                         <Accordion.Toggle as={Card.Header} eventKey="0">
-                                            <span className="card-head text-primary"> <i className="mr-2 align-middle ri-lg ri-user-fill "></i> Müşteri Bilgileri</span> <i className="fa text-primary fa-chevron-right"></i>
+                                            <span className="card-head text-primary"> <i className="mr-2 align-middle ri-lg ri-user-star-fill "></i> Alıcı Bilgileri</span> <i className="fa text-primary fa-chevron-right"></i>
                                         </Accordion.Toggle>
                                         <Accordion.Collapse eventKey="0">
+                                            <Card.Body>
+                                                <div className="mb-4">
+                                                    <h5 className="caption">İsim Soyisim</h5>
+                                                    <div className="d-flex align-items-center">
+                                                        <h5> Ahmet Yazar</h5>
+                                                    </div>
+                                                </div>
+                                                <div className="mb-4">
+                                                    <h5 className="caption">Telefon No</h5>
+                                                    <div className="d-flex align-items-center">
+                                                        <h5> 0 (512) 123 12 13</h5>
+                                                    </div>
+                                                </div>
+                                                <div className="mb-4">
+                                                    <h5 className="caption">Adres</h5>
+                                                    <div className="d-flex align-items-center">
+                                                        <h5> Lorem ipsum cad. no 12/2 Tuzla/İstanbul</h5>
+                                                    </div>
+                                                </div>
+                                                
+                                                
+                                            </Card.Body>
+                                        </Accordion.Collapse>
+                                    </Card>
+                                    <Card>
+                                        <Accordion.Toggle as={Card.Header} eventKey="1">
+                                            <span className="card-head text-primary"><i className="mr-2 align-middle ri-lg ri-user-fill "></i>Gönderici Bilgileri</span><i className="fa text-primary fa-chevron-right"></i>
+                                        </Accordion.Toggle>
+                                        <Accordion.Collapse eventKey="1">
                                             <Card.Body>
                                                 <div className="mb-4">
                                                     <h5 className="caption">İsim Soyisim</h5>
@@ -50,81 +79,45 @@ function CustomerDetail() {
                                                     </div>
                                                 </div>
                                                 <div className="mb-4">
-                                                    <h5 className="caption">Şirket Tipi</h5>
+                                                    <h5 className="caption">Tel No</h5>
                                                     <div className="d-flex align-items-center">
-                                                        <h5> Şahıs Şirketi</h5>
+                                                        <h5> 0 (512) 123 12 13</h5>
                                                     </div>
                                                 </div>
                                                 <div className="mb-4">
-                                                    <h5 className="caption">Şirket Adı</h5>
-                                                    <div className="d-flex align-items-center">
-                                                        <h5> Yıldırım Ticaret Turizm</h5>
-                                                    </div>
-                                                </div>
-                                                <div className="media-body mb-4">
-                                                    <h5 className="caption">Telefon No</h5>
-                                                    <div className="d-flex align-items-center">
-                                                        <h5> 0(567) 123 12 13</h5>
-                                                    </div>
-                                                </div>
-                                                <div className="media-body mb-4">
-                                                    <h5 className="caption">E-Posta</h5>
-                                                    <div className="d-flex align-items-center">
-                                                        <h5> burakyildirim@eposta.com.tr</h5>
-                                                    </div>
-                                                </div>
-                                                <div className="media-body mb-4">
                                                     <h5 className="caption">Adres</h5>
                                                     <div className="d-flex align-items-center">
-                                                        <h5> Lorem mah. ipsum cad. dolor sokak No:12/7 Tuzla/Istanbul</h5>
+                                                        <h5> Lorem ipsum cad. no 12/2 Tuzla/İstanbul </h5>
                                                     </div>
                                                 </div>
-                                                <div className="media-body">
-                                                    <div className="d-flex justify-content-between">
-                                                        <h5 className="caption">Müşteri Kategorisi</h5>
-                                                        <p className="caption text-primary">Düzenle</p>
 
-                                                    </div>
 
-                                                    <div className="d-flex align-items-center">
-                                                        <i className="ri-checkbox-blank-circle-fill text-danger"></i><h5 className="mb-0 ml-2" > Kırmızı</h5>
-                                                    </div>
-                                                </div>
+                                                
+
                                             </Card.Body>
                                         </Accordion.Collapse>
                                     </Card>
                                     <Card>
-                                        <Accordion.Toggle as={Card.Header} eventKey="1">
-                                            <span className="card-head text-primary"><i className="mr-2 align-middle ri-lg ri-newspaper-fill "></i>Fatura Bilgileri</span><i className="fa text-primary fa-chevron-right"></i>
+                                        <Accordion.Toggle as={Card.Header} eventKey="2">
+                                            <span className="card-head text-primary"><i className="mr-2 align-middle ri-lg ri-truck-fill "></i>Paket Bilgisi</span><i className="fa text-primary fa-chevron-right"></i>
                                         </Accordion.Toggle>
-                                        <Accordion.Collapse eventKey="1">
+                                        <Accordion.Collapse eventKey="2">
                                             <Card.Body>
                                                 <div className="mb-4">
-                                                    <h5 className="caption">İşletme Adı</h5>
+                                                    <h5 className="caption">Paket Boyutu</h5>
                                                     <div className="d-flex align-items-center">
-                                                        <h5> Yıldırım Ticaret Turizm</h5>
+                                                        <h5> 120x30x10(cm) - 12 Kg</h5>
                                                     </div>
                                                 </div>
                                                 <div className="mb-4">
-                                                    <h5 className="caption">Vergi Dairesi</h5>
+                                                    <h5 className="caption">Fiyat</h5>
                                                     <div className="d-flex align-items-center">
-                                                        <h5> Anadolu Kurumlar</h5>
+                                                        <h5> 1.200TL</h5>
                                                     </div>
                                                 </div>
-                                                <div className="mb-4">
-                                                    <h5 className="caption">Vergi No</h5>
-                                                    <div className="d-flex align-items-center">
-                                                        <h5> 3334 111 223</h5>
-                                                    </div>
-                                                </div>
+                                                
 
-
-                                                <div className="media-body mb-4">
-                                                    <h5 className="caption">Adres</h5>
-                                                    <div className="d-flex align-items-center">
-                                                        <h5> Lorem mah. ipsum cad. dolor sokak No:12/7 Tuzla/Istanbul</h5>
-                                                    </div>
-                                                </div>
+                                                
 
                                             </Card.Body>
                                         </Accordion.Collapse>
@@ -134,8 +127,7 @@ function CustomerDetail() {
 
                                 </Accordion>
                             </div>
-                            <RecentShipment />
-                            <AccountActivities />
+                            
 
                         
 
@@ -157,4 +149,4 @@ function CustomerDetail() {
     )
 }
 
-export default CustomerDetail;
+export default ShipmentDetail;
