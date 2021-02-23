97b5380b260e39e92c48008c19f9704633cc55fb
Seçkin Sözen <31372410+SeckinSozen@users.noreply.github.com>
2021-02-23 09:37:27 +0300

    Updated Responsive

--- src/jsx/layout/header1.jsx
+++ src/jsx/layout/header1.jsx
@@ -16,20 +16,30 @@ function Header1() {
                                 <Navbar bg="light" expand="lg">
                                     <Link className="navbar-brand" to={'/'}><img src={require('./../../images/logo.svg').default} alt="" /></Link>
                                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                                    <Navbar.Collapse>
-                                        <Nav className="navigation">
+                                    <Navbar.Collapse className="justify-content-between">
+                                        <Nav className="navigation  ">
                                             
-                                            <Nav.Item><Link className="text-primary" to={'/how-it-works'}>How it Works</Link></Nav.Item>
-                                            <Nav.Item><Link className="text-primary" to={'/wallet'}>FAQ</Link></Nav.Item>
+                                            <Nav.Item><Link className="text-primary" to={'/how-it-works'}>Nasıl Gönderirim?</Link></Nav.Item>
+                                            <Nav.Item><Link className="text-primary" to={'/faq'}>SSS</Link></Nav.Item>
+                                            
+                                            
+                                                
+
+                                            
+                                        </Nav>
+                                        <Nav className="navigation ">
+                                            
+                                            <Nav.Item><Link className="btn text-primary" to={'/signin'}>Giriş Yap</Link></Nav.Item>
+                                            <Nav.Item className="ml-0"><Link className="btn  btn-primary" to={'/signin'}>Kayıt Ol</Link></Nav.Item>
+                                            
+                                                
+
+                                            
                                         </Nav>
 
                                     </Navbar.Collapse>
 
-                                    <div className="signin-btn">
-                                        <Link className="btn text-primary" to={'/signin'}>Login</Link>
-                                        <Link className="btn btn-primary" to={'/signin'}>Sign up</Link>
-                                        
-                                    </div>
+                                    
                                 </Navbar>                                
                             </div>
                         </div>
